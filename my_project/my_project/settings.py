413d3e0a07da4f4665077b58563b29f00e489cb4
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-05-06 21:28:00 +0530

    Do changes for heroku deployement

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&3hs&y+jx*yv&5%xp2nn1-x2180xp9%$#(zryp^!ipn3o&odh-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
